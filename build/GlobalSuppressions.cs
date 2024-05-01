4834924cf87dcfa286c94af8ee930798c0163a45
Mohamed Asaker <asakermohamad471@icloud.com>
2024-05-01 11:19:50 -0700

    Fixed styling and added suppressions to files under Build directory (#4)

--- build/GlobalSuppressions.cs
+++ build/GlobalSuppressions.cs
@@ -14,3 +14,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.SpecialRules", "SA0001:XmlCommentAnalysisDisabled", Justification = "Documentation is not generated in the CI.")]
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements should appear in the correct order", Justification = "Reviewed.")]
 [assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "Reviewed.")]
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1009:Closing parenthesis should be spaced correctly", Justification = "Reviewed.", Scope = "member", Target = "~F:VersionHelper.Version")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "Reviewed")]
